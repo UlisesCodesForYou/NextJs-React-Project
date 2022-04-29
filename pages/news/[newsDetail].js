b88fd65a853bc4468cfba4acc46d37b373541d53
UlisesCodesForYou <76757797+UlisesCodesForYou@users.noreply.github.com>
2022-04-28 21:20:33 -0700

    This practice project is finished.

--- pages/news/[newsDetail].js
+++ pages/news/[newsDetail].js
@@ -1,4 +1,9 @@
+import { useRouter } from "next/router";
+
 const DetailPage = () => {
+  const router = useRouter();
+
+  router.query.newsDetail;
   return <h1>This is the details page</h1>;
 };
 
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
 const NewsPage = () => {
-    return <h1>News Page</h1>
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <Link href="/news/this-is-the-url">
+          <li>NextJs is kinda confusing</li>
+        </Link>
+
+        <li>This is a link to another page</li>
+      </ul>
+    </>
+  );
 };
 
-export default NewsPage; 
+export default NewsPage;
